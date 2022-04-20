fa7214224b242e43df3004eeca51ded6147769e3
patri <patri@DESKTOP-Q73S40N>
2022-04-20 15:27:23 -0300

    primer commit

---
+++ ProyectDemo/src/main/java/ar/edu/unju/fi/ProyectDemo/ProyectDemoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ProyectDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectDemoApplication.class, args);
+	}
+
+}
---
+++ ProyectDemo/src/test/java/ar/edu/unju/fi/ProyectDemo/ProyectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ProyectDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
